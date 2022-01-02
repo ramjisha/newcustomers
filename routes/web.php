a2e3c9c3394137034fd542db169e015feb4d1f8b
ramjisha <crramji004@gmail.com>
2022-01-02 10:20:06 +0530

    Authenticatin pending

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/register');
 });
+Route::get('/home', function () {
+    return view('welcome');
+})->middleware(['auth']);
